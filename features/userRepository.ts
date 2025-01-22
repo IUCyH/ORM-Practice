1fd52a78f55b0104de7b4035c5d4cb2219c0a4d7
Jaemin <ghdckdwn8456@gmail.com>
2025-01-22 13:46:18 +0900

    쿼리 변경

--- features/userRepository.ts
+++ features/userRepository.ts
@@ -7,7 +7,7 @@ export default class UserRepository {
     async getUser(id: number) {
         const builder = this.entityManager.createQueryBuilder(User, "user");
         const query = builder
-            .leftJoin("user.links", "links")
+            .leftJoinAndSelect("user.links", "links")
             .addSelect("links.link")
             .where("user.id = :id", { id: id });
 
