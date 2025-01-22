8a2994b32c98ad81244832eaf5e6e7f1be3bbd32
Jaemin <ghdckdwn8456@gmail.com>
2025-01-22 12:30:33 +0900

    orm 설정파일 수정

--- ormconfig.ts
+++ ormconfig.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
         timezone: "Asia/Seoul",
         dateStrings: true
     },
-    entities: ["dist/entities/**/*.js"],
+    entities: [__dirname + "/entities/**/*.js"],
     subscribers: [],
     migrations: []
 });
