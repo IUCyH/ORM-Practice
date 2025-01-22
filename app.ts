66b65fff089f4c6583cfbea91fe3acb4d5a3e4a3
Jaemin <ghdckdwn8456@gmail.com>
2025-01-22 12:04:02 +0900

    호스트 변경

--- app.ts
+++ app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/users", userRouter);
 
 const PORT = 8080;
-const HOST = "127.0.0.1";
+const HOST = "0.0.0.0";
 app.listen(PORT, HOST, () => {
   console.log(`Server running at ${HOST}:${PORT}`);
 });
